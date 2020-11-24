285231c37895fea78f26c9b2e3a5082a94b87dd6
karthi861 <74948463+karthi861@users.noreply.github.com>
2020-11-24 00:26:06 -0800

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,26 @@
+#Create three variables (a,b,c) to same value of any integer & do the following
+#a)	Divide a by 10
+#b)	Multiply b by 50
+#c)	Add c value by 60
+ a=b=c=10
+>>> print(a/10)
+1.0
+>>> print(b*50)
+500
+>>> print(c+60)
+70
+#Create a String variable of 5 characters  and replace the 3rd character with G
+>>> str = "karthi"
+>>> k = str.replace("h","G")
+>>> print(k)
+#Create two values (a,b) of int,float data type & convert the vise versa, Hint : convert a from int to float datatype & b from float to int datatype 
+ >>> a,b=10,7.8
+>>> print(int(a))
+10
+>>> print(float(a))
+10.0
+>>> print(int(b))
+7
+>>> print(float(b))
+7.8
+
